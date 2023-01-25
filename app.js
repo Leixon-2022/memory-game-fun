801cbcffa0d8bfd7d43a0e1fa494a9e926db6ff7
Timthom <monsson47thomas@gmail.com>
2023-01-25 14:33:50 +0100

    Mera kommentarer

--- app.js
+++ app.js
@@ -7,51 +7,53 @@ const cards = document.querySelectorAll('.memory-card');
 let hasFlippedCard = false;
 let firstCard, secondCard;
 
+
 function flipCard() {
-    //3: Vi ändrar toggle til add
+
+    //2: Vi ändrar toggle till add
     this.classList.add('flip');
-
-    if (!hasFlippedCard) {
-      // first click
-      hasFlippedCard = true;
-      firstCard = this;
-  
-      return;
-    }
-  
-    // second click
-    hasFlippedCard = false;
+    //2: Nu kan vi hålla reda på om vi har vänt kort
+   if (!hasFlippedCard) {
+        hasFlippedCard = true;
+       firstCard = this;
+       return;
+   }
     secondCard = this;
-
+    
+    hasFlippedCard = false;
+     //3: Vi kallar funktionen här
     checkForMatch();
-
 }
 
+//3: Vi checkar om korten matchar här
 function checkForMatch() {
+      if (firstCard.dataset.framework === secondCard.dataset.framework) {   
+            disableCards();
+            return;
+          }
+     
+       unflipCards();
+       
+       // Ett elegantare sätt att skriva denna kod är med en 
+       // Ternary-operator. Då ser samma kod ovan ut som såhär.
+  /*      let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+        isMatch ? disableCards() : unflipCards(); */
+   
+   }
 
-/*     if (firstCard.dataset.framwork === secondCard.dataset.framework) {
-        disableCards();
-        return;
-    }
-    unFlipCards(); */
-
-// Överkurs: Det nedre uttrycket är samma som utrycket ovan och kallas för ternaryoperator
-    let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
-    isMatch ? disableCards() : unFlipCards();
-
-}
-
-function disableCards() {
+   function disableCards() {
     firstCard.removeEventListener('click', flipCard);
     secondCard.removeEventListener('click', flipCard);
-  }
+    
+}
+    
   
-  function unFlipCards() {
-    setTimeout(() => {
-      firstCard.classList.remove('flip');
-      secondCard.classList.remove('flip');
-    }, 800);
-  }
+function unflipCards() {
+       setTimeout(() => {
+         firstCard.classList.remove('flip');
+           secondCard.classList.remove('flip');
+       }, 800);
+}
 
 
 cards.forEach(card => card.addEventListener('click', flipCard));
